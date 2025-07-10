776e5db35f7efcca0ffe0731f60b81111a542012
Eugenio <223730@ids.upchiapas.edu.mx>
2025-07-10 09:21:46 -0600

    chore(routing): initialize react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 import './index.css'
-import App from './App.tsx'
+import { ThemeProvider } from "./core/theme/components/theme-provider"
+import { AuthPage } from "@/features/auth/pages/AuthPage"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AuthPage />,
+  },
+
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
